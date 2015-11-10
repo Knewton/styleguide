3b4c278d26732e6c669b143eed23073b0fb5f343
Remi Carton <remi@knewton.com>
2015-11-10 11:48:09 -0500

    Preprocessor uses babel instead of react tools

--- tests/preprocessor.js
+++ tests/preprocessor.js
@@ -1,11 +1,15 @@
-var ReactTools = require('react-tools');
+var babel = require('babel-core');
 
 module.exports = {
     process: function(src, file) {
         // Run JSX transform only on JSX and React test files
         if (/\.jsx$/.test(file) || /-test.js$/.test(file)) {
-            return ReactTools.transform(src);
+            var babelOptions = {
+                presets: ['es2015', 'react']
+            };
+            return babel.transform(src, babelOptions);
         }
         return src;
     }
 };
+
